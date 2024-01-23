8811985041fbb125a927672991f55385e5758e19
Tomi\Tomislav <rusevtomy@gmail.com>
2024-01-24 01:51:10 +0200

    Added Swagger

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from app.extensions import db
 from .view.task_view import tasks_blueprint
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:wp@localhost:8083/taskmanagementdb'
@@ -14,6 +16,26 @@ with app.app_context():
     db.create_all()
 
 app.register_blueprint(tasks_blueprint)
+
+@app.route('/api/spec')
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/api/spec'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "My API"
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
     app.run(debug=True)
